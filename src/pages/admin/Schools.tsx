90db48c0be27d0a201cec0996b71ad5c507116d7
Agwara <nnaemekaagwara@gmail.com>
2025-03-28 15:43:03 +0100

    worked on course form

--- src/pages/admin/Schools.tsx
+++ src/pages/admin/Schools.tsx
@@ -72,7 +72,7 @@ export default function Schools() {
                   outline-0 focus:outline-none flex justify-center items-center gap-x-[10px]"
               >
               <RiUploadCloud2Line className="w-5 h-5 text-[#1E1E1E]"  />
-              <p>Upload CSV</p>
+              <p>Bulk Uploads</p>
             </button>
 
             <button 
