d94f196a6844337ecb8f08cc2a189b397f85df42
Agwara <nnaemekaagwara@gmail.com>
2025-03-31 17:48:30 +0100

    Completed bulk delete of schools

--- src/services/schools.ts
+++ src/services/schools.ts
@@ -18,10 +18,44 @@ export interface School {
 
 export const getSchools = async (token: string) => {
   try {
-    // const schools = localStorage.getItem('schools')
-    // if(schools) return JSON.parse(schools)
-
     const response = await api.get("/school/get/all", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data as School[];
+  } catch (err: any) {
+        if (err.response && err.response.data && err.response.data.errors) {
+       
+          const errors = err.response.data.errors;
+  
+          errors.forEach((error: { message: string }) => {
+            if (error.message) {
+              toast.error(error.message);
+            }
+          });
+        }
+        if(err.response && err.response.data) {
+  
+          toast.error(err.response.data.error)
+        }
+  
+        if (
+          err.response &&
+          err.response.data.message &&
+          !err.response.data.errors
+        ) {
+          toast.error(err.response.data.message);
+        }
+        throw err;
+      } 
+};
+
+
+export const deleteSchools = async (schoolList: string[], token: string) => {
+  try {
+    const response = await api.delete("/bulk/bulk-delete-schools",{
+      data: {"schoolIds": schoolList},
       headers: {
         Authorization: `Bearer ${token}`,
       },
