a40d1a08a44bfb1756ec98fa5da445201c72831d
Agwara <nnaemekaagwara@gmail.com>
2025-05-12 17:39:50 +0100

    fixed zindex issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -134,7 +134,7 @@ export default function Home() {
   }
   return (
     <div className="relative h-screen w-full bg-[#FCFCFD]">
-      <div className="bg-[#FCFCFD] flex justify-between p-5 border-b border-b-[#DDDDDD] sticky top-0 z-[10]">
+      <div className="bg-[#FCFCFD] flex justify-between p-5 border-b border-b-[#DDDDDD] sticky top-0 z-[1000]">
         <div>
           <h1 className="text-[20px] font-semibold text-[#101828]">Welcome to Aligntraits</h1>
           <p className="text-[12px] font-normal text-[#999999]">Find your career path today!</p>
