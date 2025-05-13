f4a2f004754375cfad996975f838f5436de15d23
Agwara <nnaemekaagwara@gmail.com>
2025-05-13 13:47:21 +0100

    fixed landing page controls

--- src/components/dashboard/CourseCard.tsx
+++ src/components/dashboard/CourseCard.tsx
@@ -1,8 +1,14 @@
 import courseIcon from "../../assets/Imageholder.svg"
+import { Course } from '../../types/course.types';
 import courseIconTwo from "../../assets/imageHolderTwo.svg"
 import locationIcon from "../../assets/locationIcon.svg"
 
-const CourseCard = () => {
+interface CoursesProps {
+  courseItem: Course;
+}
+
+const CourseCard = ({courseItem}: CoursesProps) => {
+  // console.log("courseItem: ", courseItem)
   return (
     <div className="w-[300px] size-max rounded-md border-[2px] border-[#EAECF0] p-[8px] flex flex-col gap-y-[10px]">
       <div className="w-[100%] h-[180px] rounded-md">
@@ -13,12 +19,14 @@ const CourseCard = () => {
         <img alt="course Icon two" src={courseIconTwo} className="h-[35px] w-[35px] mt-[5px]" />
 
         <div>
-          <p className="text-[#000000] font-semibold text-[16px]">Computer Science</p>
-          <p className="text-[#999999] font-medium text-[12px]">Convenant University</p>
+          <p className="text-[#000000] font-semibold text-[16px]">{courseItem.title}</p>
+          <p className="text-[#999999] font-medium text-[12px]">{courseItem.university?.name}</p>
 
           <div className="flex gap-x-[5px]">
             <img src={locationIcon} alt="Location Icon" className="" />
-            <p className="text-[#007BFF] text-[12px] font-normal">Ogun / Nigeria</p>
+            <p className="text-[#007BFF] text-[12px] font-normal">
+              {courseItem.university?.region}/{courseItem.university?.country}
+            </p>
           </div>
         </div>
       </div>
@@ -30,11 +38,11 @@ const CourseCard = () => {
 
       <div className="flex justify-between">
         <div className="bg-[#E6F2FF] rounded-lg h-[35px] w-[120px] flex justify-center items-center">
-          <p className="text-[#007BFF] text-[14px] font-medium">N2,000,000</p>
+          <p className="text-[#007BFF] text-[14px] font-medium">{courseItem.acceptanceFeeCurrency} {courseItem.acceptanceFee}</p>
         </div>
 
         <div className="bg-[#E6F2FF] rounded-lg h-[35px] w-[130px] flex justify-center items-center">
-          <p className="text-[#007BFF] text-[14px] font-medium">Full Scholarship</p>
+          <p className="text-[#007BFF] text-[14px] font-medium">{courseItem.scholarship}</p>
         </div>
       </div>
 
