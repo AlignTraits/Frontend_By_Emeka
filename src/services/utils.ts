f61ef0472b27f0396e8c4439488f16d44aceb6ed
Agwara <nnaemekaagwara@gmail.com>
2025-06-16 13:17:45 +0100

    worked on checkEligibility API

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -52,4 +52,41 @@ export const sendCareerPath = async (data: any) => {
   }
 }
 
+export const checkEligibility = async (data: any) => {
+  try {
+    const response = await api.post("/server/eligible/answers", data)
+
+    console.log("response: from API", response) 
+
+    if(response.data.status == 403) throw new ResponseError(response)
+    if(response.data.status == 500) throw new ResponseError(response)  
+    return response.data;
+  } catch (err: any) {
+    if (err.response && err.response.data && err.response.data.errors) {
+      
+      const errors = err.response.data.errors;
+
+      errors.forEach((error: { message: string }) => {
+        if (error.message) {
+          toast.error(error.message);
+        }
+      });
+    }
+    if(err.response && err.response.data) {
+
+      toast.error(err.response.data.error)
+    }
+
+    if (
+      err.response &&
+      err.response.data.message &&
+      !err.response.data.errors
+    ) {
+      toast.error(err.response.data.message);
+    }
+
+    throw err;
+  }
+}
+
 
