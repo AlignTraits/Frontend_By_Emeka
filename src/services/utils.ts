835f8233032b75069acfad9203a75dbfd6b048f1
Agwara <nnaemekaagwara@gmail.com>
2025-08-05 16:40:54 +0100

    new update

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -171,6 +171,41 @@ export const deleteAcademicRecords = async (id: string) => {
   }
 }
 
+export const addDebitCard = async (data: any) => {
+  try {
+    const response = await api.post("/monthly/payment/add-direct-debit", data)
+
+    if(response.data.status == 403) throw new ResponseError(response)
+    if(response.data.status == 500) throw new ResponseError(response)  
+    return response.data;
+  } catch (err: any) {
+    if (err.response && err.response.data && err.response.data.errors) {
+      
+      const errors = err.response.data.errors;
+
+      errors.forEach((error: { message: string }) => {
+        if (error.message) {
+          toast.error(error.message);
+        }
+      });
+    }
+    if(err.response && err.response.data) {
+
+      toast.error(err.response.data.error)
+    }
+
+    if (
+      err.response &&
+      err.response.data.message &&
+      !err.response.data.errors
+    ) {
+      toast.error(err.response.data.message);
+    }
+
+    throw err;
+  }
+}
+
 export const getAcademicRecords = async () => {
   try {
     const response = await api.get("/admission-logic/academic-records")
