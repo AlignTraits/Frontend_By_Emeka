20a4a297e63bc46a4c2351a65d1343df399a1c96
Agwara <nnaemekaagwara@gmail.com>
2025-08-07 16:07:26 +0100

    new update

--- src/components/dashboard/Pathway/RecommendationResults.tsx
+++ src/components/dashboard/Pathway/RecommendationResults.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { SlGraph } from "react-icons/sl";
+import { useAuth } from '../../../contexts/useAuth';
 
 // const recommendations = new Array(8).fill({
 //   title: "Business Intelligence Specialist",
@@ -11,6 +12,10 @@ interface RecommendationProps {
 }
 
 export default function RecommendationResults({setViewState}: RecommendationProps) {
+  const { user } = useAuth();
+
+  console.log("user: ", user);
+
   return (
     <div className="w-full max-w-2xl mx-auto mt-[50px] bg-white border-[1px] border-[#ccc] shadow-md rounded-xl p-6">
       <h2 className="text-lg font-semibold text-[#101828] mb-6 flex items-center">
@@ -18,21 +23,18 @@ export default function RecommendationResults({setViewState}: RecommendationProp
         Recommendation Survey Results
       </h2>
 
-      {/* <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-6">
-        {recommendations.map((rec, index) => (
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-6">
+        {user?.careerResults.recommendedCareers && user.careerResults.recommendedCareers.map((rec: string, index:number) => (
           <div
             key={index}
             className="bg-white cursor-pointer border border-gray-100 shadow-sm rounded-lg p-4 hover:shadow-md transition"
           >
             <h3 className="text-[16px] font-bold text-[#212529] mb-1">
-              {rec.title}
+              {rec}
             </h3>
-            <p className="text-[14px] text-[#757575] font-semibold cursor-pointer hover:underline">
-              {rec.description}
-            </p>
           </div>
         ))}
-      </div> */}
+      </div>
 
       <div className="text-center">
         <button onClick={() => setViewState(1)} className="bg-[#004085] hover:bg-blue-800 text-white font-medium py-4 px-5 rounded-2xl transition">
