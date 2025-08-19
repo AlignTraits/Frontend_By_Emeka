7c9fecb26075d0923b2d334ef80d5f97a8a440c2
Agwara <nnaemekaagwara@gmail.com>
2025-08-19 15:20:29 +0100

    new fix

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -37,33 +37,32 @@ export default function DashboardLayout() {
             return navigate("/onboarding-form")
           }
 
-          let tempDataTwo = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData") as string) : {ok: true};
+          // For new users, that filled their career pathway outside of login can they have a pop up on 
+          // their first login to redirect them to career path?
+
+          if (!response.data?.isCareerPathChecked) {
+            return setShowModal(true);
+          }
+
+          // let tempDataTwo = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData") as string) : {ok: true};
 
           // If this is a first time user, redirect to the career pathway
           // During the career pathway flow, I set userData in localStorage;
 
-          if (tempDataTwo && !tempDataTwo?.ok) {
-            // toast.success("Welcome back to your Career Pathway");
+          // if (tempDataTwo && !tempDataTwo?.ok) {
 
-            toast.success("Welcome back to your Career Pathway");
+          //   toast.success("Welcome back to your Career Pathway");
 
-            return setTimeout(() => {
-              navigate("/dashboard/career-pathway")
-            }, 1500);
-          }
+          //   return setTimeout(() => {
+          //     navigate("/dashboard/career-pathway")
+          //   }, 1500);
+          // }
  
           if (!response.data?.careerResults) {
             toast.success("Take our quiz and check your career path");
             return setTimeout(() => {
               navigate("/career-recommedation");
             }, 1500);
-          }
-
-          // For new users, that filled their career pathway outside of login can they have a pop up on 
-          // their first login to redirect them to career path?
-
-          if (!response.data?.isCareerPathChecked) {
-            return setShowModal(true);
           }
 
         }
