9681a7aabc3e8f339a1794a9d92f25951ff54859
Agwara <nnaemekaagwara@gmail.com>
2025-11-30 19:30:01 +0100

    added explore path

--- src/pages/dashboard/CareerPath.tsx
+++ src/pages/dashboard/CareerPath.tsx
@@ -1,89 +1,130 @@
-
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 // import Construction from '../../assets/dashboard/images/construction.png'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { FaArrowLeftLong } from "react-icons/fa6";
-import { useAuth } from '../../contexts/useAuth';
-import RecommendationResults from '../../components/dashboard/Pathway/RecommendationResults';
-import CourseList from '../../components/CareerPathway/CourseList';
-import Other from '../../components/CareerPathway/Other';
-import { upDateUserProfile } from '../../services/auth.service';
-
+import { useAuth } from "../../contexts/useAuth";
+import RecommendationResults from "../../components/dashboard/Pathway/RecommendationResults";
+import CourseList from "../../components/CareerPathway/CourseList";
+import Other from "../../components/CareerPathway/Other";
+import ExplorePath from "../../components/CareerPathway/ExplorePath";
+import { upDateUserProfile } from "../../services/auth.service";
+import { getCareerPath } from "../../services/utils";
 
 export default function CareerPath() {
-  const {setPageDesc, token, user} = useAuth()
-  const [courseFilter, setCourseFilter] = useState("")
+  const { setPageDesc, token, user } = useAuth();
+  const [courseFilter, setCourseFilter] = useState("");
   const [viewState, setViewState] = useState(0);
-  const [showDetails, setShowDetails] = useState(false)
+  const [showDetails, setShowDetails] = useState(false);
+  const [explorePath, setExplorePath] = useState("");
+  const [careerPath, setCareerPath] = useState<any>(null);
 
   const navigate = useNavigate();
+
+  const handlegGetCareerPath = async () => {
+    try {
+      const response = await getCareerPath();
+      console.log("handlegGetCareerPath: ", response);
+      if (response.ok) {
+        setCareerPath(response.data);
+      }
+    } catch (err: any) {
+      console.log("error: ", err);
+    }
+  };
 
   useEffect(() => {
     setPageDesc({
       desc: "Here’s a list of career pathway for you.",
-      title: "Career Pathway"
-    })
+      title: "Career Pathway",
+    });
 
     if (!user?.isCareerPathChecked) {
       updateUser();
     }
-  }, [])
 
+    handlegGetCareerPath();
+  }, []);
 
   const handleNav = () => {
     if (viewState === 0) {
-      navigate(-1)
+      navigate(-1);
     } else {
-      setViewState((prev) => prev - 1);
+      setViewState(0);
     }
-  }
+  };
 
   const updateUser = async () => {
     const updateData = {
-      isCareerPathChecked: true
-    }
+      isCareerPathChecked: true,
+    };
     try {
-      const response = await upDateUserProfile(
-        updateData,
-        token as string,
-      );
-      console.log("response: ", response)
-
+      const response = await upDateUserProfile(updateData, token as string);
+      return response;
     } catch (error) {
-      
       console.error("Error updating user profile", error);
-    } 
-  }
+    }
+  };
 
   const renderState = () => {
-    switch (viewState) { 
+    switch (viewState) {
       case 0:
-        return <RecommendationResults setViewState={setViewState} />;
+        return (
+          <RecommendationResults
+            setExplorePath={setExplorePath}
+            setViewState={setViewState}
+          />
+        );
       case 1:
-        return <Other setViewState={setViewState} setCourseFilter={setCourseFilter} showDetails={showDetails} setShowDetails={setShowDetails} />;  
+        return (
+          <Other
+            setViewState={setViewState}
+            setCourseFilter={setCourseFilter}
+            showDetails={showDetails}
+            setShowDetails={setShowDetails}
+          />
+        );
       case 2:
-        return <CourseList courseFilter={courseFilter} setShowDetails={setShowDetails} showDetails={showDetails} />;  
+        return (
+          <CourseList
+            courseFilter={courseFilter}
+            setShowDetails={setShowDetails}
+            showDetails={showDetails}
+          />
+        );
+      case 3:
+        return (
+          <ExplorePath
+            setViewState={setViewState}
+            careerPath={careerPath}
+            explorePath={explorePath}
+          />
+        );
       default:
-        return <RecommendationResults setViewState={setViewState} />;  
+        return (
+          <RecommendationResults
+            setExplorePath={setExplorePath}
+            setViewState={setViewState}
+          />
+        );
     }
-  }
+  };
 
   return (
     <div>
       <div className="w-full h-full p-5">
-        {
-          !showDetails &&         
-          <button onClick={handleNav} className="flex gap-x-[10px] items-center">
+        {!showDetails && (
+          <button
+            onClick={handleNav}
+            className="flex gap-x-[10px] items-center"
+          >
             <FaArrowLeftLong className="text-[#004085]" />
             <p className="text-[#004085]">Back to Explore</p>
           </button>
-        }
-        {
-          renderState()
-        }
+        )}
+        {renderState()}
       </div>
 
-      <div className='h-[70px] w-[30px]'></div>
+      <div className="h-[70px] w-[30px]"></div>
     </div>
   );
 }
--- src/services/utils.ts
+++ src/services/utils.ts
@@ -435,3 +435,36 @@ export const getCourseRecommendation = async (data: any) => {
     throw err;
   }
 };
+
+export const getCareerPath = async () => {
+  try {
+    const response = await api.get(`/career`);
+
+    if (response.data.status == 403) throw new ResponseError(response);
+    if (response.data.status == 500) throw new ResponseError(response);
+    return response.data;
+  } catch (err: any) {
+    if (err.response && err.response.data && err.response.data.errors) {
+      const errors = err.response.data.errors;
+
+      errors.forEach((error: { message: string }) => {
+        if (error.message) {
+          toast.error(error.message);
+        }
+      });
+    }
+    if (err.response && err.response.data) {
+      toast.error(err.response.data.error);
+    }
+
+    if (
+      err.response &&
+      err.response.data.message &&
+      !err.response.data.errors
+    ) {
+      toast.error(err.response.data.message);
+    }
+
+    throw err;
+  }
+};
