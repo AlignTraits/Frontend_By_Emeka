c3f4d37d442251e79b201986ab4c2f3e5f7a0b17
Julius <odionjulius7@gmail.com>
2026-07-24 09:50:12 +0100

    useNavigate fixed

--- src/components/CareerPathway/ExplorePath.tsx
+++ src/components/CareerPathway/ExplorePath.tsx
@@ -5,7 +5,7 @@ import {
   Share2,
   Loader2,
 } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { useRef, useState } from "react";
 import html2canvas from "html2canvas";
 
